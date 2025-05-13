5129230233a2089e7c5703d8cbb38fcad513dc1b
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-13 23:46:21 +0900

    update

---
+++ 25_jquery_menu/js/fix.js
@@ -0,0 +1,9 @@
+// TODO: id=nav のY座標取得: offset().top
+const navPos = 0
+$(window).scroll(function () {
+    if ($(window).scrollTop() > navPos) {
+        // TODO: 固定 css設定：position: fixed
+    } else {
+        // TODO: 固定解除 css設定：position: static
+    }
+});
