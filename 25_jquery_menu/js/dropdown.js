79585940470164a1ffd36407f91c0ce289440b13
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-14 16:33:36 +0900

    update

--- 25_jquery_menu/js/dropdown.js
+++ 25_jquery_menu/js/dropdown.js
@@ -22,7 +22,7 @@ $(function () {
     $navMenus.on('click', function () {
         // TODO: 親メニューの子要素 ul を slideUp() アニメーション
         // TODO: アニメーションは一旦停止
-
+        $(this).children('ul').stop().slideUp(200)
     });
 
     // スムーススクロール
