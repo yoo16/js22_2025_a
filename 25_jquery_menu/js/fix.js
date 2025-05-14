445ca56b659cae0d06fcb2392ee96744afd4ee10
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-14 17:01:36 +0900

    update

--- 25_jquery_menu/js/fix.js
+++ 25_jquery_menu/js/fix.js
@@ -23,15 +23,17 @@ $(function () {
 
     // スクロール固定
     const nav = $("#nav");
-    // TODO: nav の現在の top を取得
+    // nav の現在の top を取得
     const navPos = nav.offset().top;
 
     $(window).scroll(function () {
         // スクロール位置が nav の位置を超えたら
         if ($(window).scrollTop() > navPos) {
             // TODO: 固定 css設定：position: fixed
+            nav.css({ position: "fixed" })
         } else {
             // TODO: 固定解除 css設定：position: static
+            nav.css({ position: "static" })
         }
     });
 });
