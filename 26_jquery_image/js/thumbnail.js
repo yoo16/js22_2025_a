8476adc530de21a8e46e05ec2c41277c954a4540
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-21 15:08:20 +0900

    update

--- 26_jquery_image/js/thumbnail.js
+++ 26_jquery_image/js/thumbnail.js
@@ -1,8 +1,15 @@
 $(function () {
     // TODO: サムネイルクリック
-    // TODO: img から 画像パス src を取得
-    // TODO: メイン画像の img をフェードアウト
     // TODO: コールバックで、メイン画像の img にパスを設定してフェードイン
+    $(".thumbnail").on("click", function () {
+        console.log("サムネイルクリック");
+        // TODO: img から 画像パス src を取得
+        const src = $(this).attr("src");
+        // TODO: メイン画像の img をフェードアウト
+        $("#main-image").fadeOut(300, function () {
+
+        });
+    });
 });
 
 
