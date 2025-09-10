1e910ea1bb55d901ce5ad26fe2a6bdc22d2983ca
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-09-10 15:29:37 +0900

    update

--- 36_cookie/basic/js/user.js
+++ 36_cookie/basic/js/user.js
@@ -53,6 +53,8 @@ document.getElementById("saveBtn").addEventListener("click", () => {
 document.getElementById("deleteBtn").addEventListener("click", () => {
     // Cookie削除
     deleteCookie(KEY);
+    // document.cookie = `account_name=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+
     // 表示更新
     showCookies();
 });
